c33f9ab8e0320c2e677c5d88e21a7fbe9e5b87a6
Jason Sanford <jasonsanford@gmail.com>
2016-01-06 09:50:16 -0500

    Document PROGRESS

--- docs/event_docs.js
+++ docs/event_docs.js
@@ -7,6 +7,7 @@
  * @param {String} message The message content for the alert.
  * @returns {null}
  * @example
+ * ALERT('Warning!', 'A depth of 98 feet is high. Are you sure?')
  * // Displays an alert that looks like
  * // +-------------------------------------------+
  * // | Warning!                                  |
@@ -15,6 +16,30 @@
  * // | A depth of 98 feet is high. Are you sure? |
  * // |                                           |
  * // +-------------------------------------------+
- * ALERT('Warning!', 'A depth of 98 feet is high. Are you sure?')
  */
 function ALERT() {}
+
+
+////PROGRESS
+
+/**
+ * PROGRESS
+ * Display a non-dismissible progress message in the mobile app.
+ * @param {String} title A short title for the progress message.
+ * @param {String} message The message content for the progress alert.
+ * @returns {null}
+ * @example
+ * PROGRESS('Just a sec!', 'Searching for nearby facilities ...')
+ * // Displays an progress message that looks like
+ * // +-------------------------------------------+
+ * // | Just a sec!                               |
+ * // +-------------------------------------------|
+ * // |                                           |
+ * // | Searching for nearby facilities ...       |
+ * // |                                           |
+ * // +-------------------------------------------+
+ * @example
+ * // Call with no parameters to dismiss the progress message.
+ * PROGRESS()
+ */
+function PROGRESS() {}
