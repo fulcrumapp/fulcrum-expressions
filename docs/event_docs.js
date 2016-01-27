d2fe406a6d471ac72e2831ed5424fe495300c962
Jason Sanford <jasonsanford@gmail.com>
2016-01-27 15:15:16 -0500

    Document STORAGE

--- docs/event_docs.js
+++ docs/event_docs.js
@@ -452,3 +452,37 @@ function SETVALUE() {}
  * // Unsets any override previously set by SETREADONLY and uses the original setting from the form schema
  */
 function SETREADONLY() {}
+
+
+////STORAGE
+
+/**
+ * STORAGE
+ * Instantiates a storage object for setting and getting local storage items.
+ * @example
+ * storage = STORAGE()
+ * KEY = 'item_key'
+ * storage.setItem(KEY, 'hello world')
+ *
+ * // Sets an item in local storage, must be a string
+ * @example
+ * item = storage.getItem(KEY)
+ *
+ * // Gets an item from storage if it exists, otherwise returns null
+ * @example
+ * geom = { type: 'Point', coordinates: [-100, 40] }
+ * storage.setItem('geometry', JSON.stringify(geom))
+ *
+ * anotherGeom = JSON.parse(storage.getItem('geometry'))
+ *
+ * // Use JSON.stringify and JSON.parse to serialize and deserialize objects in local storage
+ * @example
+ * storage.removeItem(KEY)
+ *
+ * // Removes an item from storage
+ * @example
+ * storage.clear()
+ *
+ * // Removes all items from storage
+ */
+function STORAGE() {}
