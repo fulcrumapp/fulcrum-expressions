b997b3318e9add164876e8e60327e877b4446e17
Jirles <emily.jirles@gmail.com>
2018-09-20 12:59:54 -0400

    Minor edits to AND documentation, add ts-ignore lines to test suite

--- src/functions/AND.ts
+++ src/functions/AND.ts
@@ -1,15 +1,16 @@
-import _ from 'underscore';
+import { toArray, find } from 'lodash';
+
 
 /**
- * Accepts multiple expressions and returns true if both are true, and false if both or one is false
+ * Accepts an array of expressions and returns true if both are true, and false if both or one is false
  * @returns boolean value
  * @example 
  * AND('this' === 'that', 4 > 2) // returns false
  * AND('this' === 'this', 4 > 2) // returns true
  */
 
-export default function AND() : boolean | null {
-  if (arguments.length === 0) return null;
+export default function AND(...args : any[]) : boolean | null {
+  if (args.length === 0) return null;
 
-  return _.find(_.toArray(arguments), (item: any) => !item) === undefined;
+  return find(args, (item: any) => !item) === undefined;
 }
--- src/functions/__tests__/AND.test.ts
+++ src/functions/__tests__/AND.test.ts
@@ -1,6 +1,7 @@
 import AND from '../AND';
 
 test('it evaluates expressions and returns a boolean value', () => {
+  // @ts-ignore Need a value that evaluates to false
   expect(AND(5 > 2, 'hi' === 'howdy')).toEqual(false);
   expect(AND(5 > 2, 'hi' === 'hi')).toEqual(true);
 });
@@ -10,7 +11,8 @@ test('it accepts more than two expressions', () => {
 });
 
 test('it accepts one expression', () => {
-  expect(AND('this' === 'that')).toEqual(false);
+  // @ts-ignore Need a value that evaluates to false
+  expect(AND("this" === "that")).toEqual(false);
 });
 
 test('it returns null if no arguments are passed', () => {
