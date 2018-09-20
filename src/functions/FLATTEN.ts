a5af71058cdfc6a194e6063167ae2d6b23fa754b
Jirles <emily.jirles@gmail.com>
2018-09-20 10:28:00 -0400

    Add FLATTEN function and documentation

---
+++ src/functions/FLATTEN.ts
@@ -0,0 +1,13 @@
+import _ from 'underscore';
+
+/**
+ * Accepts a nested array and returns an array flattened to one level deep
+ * @returns a flattened array
+ * @example
+ * FLATTEN([1, [2, [3]]]) // returns [1, 2, 3]
+ */
+
+export default function FLATTEN(value: any[]) : any[] {
+  // return null if value is not an array, otherwise return a flattened array
+  return Array.isArray(value) ? _.flatten(value) : null;
+}
