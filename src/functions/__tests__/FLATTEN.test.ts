fbcc740d53352cd876f7b3d0c0b0e236b6e7cf5f
Jirles <emily.jirles@gmail.com>
2018-09-20 10:47:18 -0400

    Stub out bad arguments test and fix earlier spelling mistake

--- src/functions/__tests__/FLATTEN.test.ts
+++ src/functions/__tests__/FLATTEN.test.ts
@@ -9,11 +9,16 @@ test('it accepts a nested array of numbers and returns a flattened array', () =>
 test('it accepts a nested array of strings and returns a flattened array', () => {
   const testArray : any[] = ['Hello', 'World', ['foo', ['bar']]];
 
-  expect(FLATTEN(testArray)).toEqual(['Hello', 'World', 'foo', 'boo']);
+  expect(FLATTEN(testArray)).toEqual(['Hello', 'World', 'foo', 'bar']);
 });
 
 test('it accepts a nested array of mixed primitive types', () => {
   const mixedArray : any[] = ['Hello', [2, 5, ['World']]];
 
   expect(FLATTEN(mixedArray)).toEqual(['Hello', 2, 5, 'World']);
+});
+
+test('it returns an empty array unless it is passed an array', () => {
+  expect(FLATTEN(3)).toEqual([]);
+  expect(FLATTEN('Howdy')).toEqual([]);
 });
