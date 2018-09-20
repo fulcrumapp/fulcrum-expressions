128f43f3b1fbbc8d5c75ce4c451582eace5012e4
Jirles <emily.jirles@gmail.com>
2018-09-20 13:06:44 -0400

    Add ARRAY documentation

--- src/functions/ARRAY.ts
+++ src/functions/ARRAY.ts
@@ -1,6 +1,14 @@
 import FLATTEN from './FLATTEN';
-import _ from 'underscore';
 
-export default function ARRAY() : any[] {
-  return FLATTEN(_.toArray(arguments));
+/**
+ * Accepts any number of arguments and returns them as an array
+ * @param args list of items of any type
+ * @returns an array flattened to on level deep
+ * @example
+ * ARRAY(1, 2, 3) // returns [1, 2, 3]
+ * ARRAY([1, 2], '3', [4, 5]) // returns [1, 2, '3', 4, 5]
+ */
+
+export default function ARRAY(...args: any[]) : any[] {
+  return FLATTEN(args);
 }
