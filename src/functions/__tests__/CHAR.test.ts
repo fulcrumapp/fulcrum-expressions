809e00a7e4db837a00b504c13d44b11d69d16002
Jirles <emily.jirles@gmail.com>
2018-09-21 13:22:30 -0400

    Extend CHAR test suite

--- src/functions/__tests__/CHAR.test.ts
+++ src/functions/__tests__/CHAR.test.ts
@@ -5,3 +5,14 @@ test('returns the character given the char code', () => {
   expect(CHAR(90)).toEqual('Z');
   expect(CHAR(1337)).toEqual('Թ');
 });
+
+test('it accepts numbers as strings', () => {
+  expect(CHAR('65')).toEqual('A');
+  expect(CHAR('90')).toEqual('Z');
+  expect(CHAR('1337')).toEqual('Թ');
+});
+
+test('it returns a null character value given a non-number', () => {
+  expect(CHAR('A')).toEqual("\u0000");
+  expect(CHAR('a2')).toEqual("\u0000");
+});
