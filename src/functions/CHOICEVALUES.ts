f10c3d74d88fe3c833f2747f85856ed49512b80a
Jirles <emily.jirles@gmail.com>
2018-09-21 17:29:09 -0400

    Add documentation to CHOICEVALUES, rearrange overloads

--- src/functions/CHOICEVALUES.ts
+++ src/functions/CHOICEVALUES.ts
@@ -2,10 +2,18 @@ import ISBLANK from './ISBLANK';
 import { isUndefined, compact, concat } from 'lodash';
 import { FormFieldValues as ChoiceFieldValue} from '../values';
 
+/**
+ * Converts a choicefield object to an array with null values and blank strings removed
+ * @param field ChoiceFieldValue: {choice_values: [ ... ], other_values: [ ... ]}
+ * @returns array
+ * 
+ * @example
+ * CHOICEVALUES({choice_values: ['a', 'b'], other_values: ['c', 'd']}) // returns [ 'a', 'b', 'c', 'd' ]
+ */
 export default function CHOICEVALUES(field: ChoiceFieldValue): string[]
+export default function CHOICEVALUES(field: any): any[]
 export default function CHOICEVALUES(): undefined
-export default function CHOICEVALUES(field: any) : any[]
-export default function CHOICEVALUES(field?: any) : any[]|undefined {
+export default function CHOICEVALUES(field?: any): any[]|undefined {
   if(isUndefined(field)) return undefined;
   if (ISBLANK(field)) return [];
 
