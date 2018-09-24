19688357525d2fcf662b9e1d8f8fdbbe449d5732
Jirles <emily.jirles@gmail.com>
2018-09-24 14:37:39 -0400

    Add documentation to CODE

--- src/functions/CODE.ts
+++ src/functions/CODE.ts
@@ -1,6 +1,16 @@
 import { isNumber, isString } from "lodash"
 import { MaybeString } from "../primitives"
 
+/**
+ * Returns numeric code for first character in passed in string
+ * @param str string
+ * @returns numeric code
+ * @example
+ * CODE("A") // returns 65
+ * CODE("apple") // returns 97
+ * CODE(9) // returns 57
+ */
+
 export default function CODE(str: MaybeString): number
 export default function CODE(str: number): number
 export default function CODE(str: Object): number
