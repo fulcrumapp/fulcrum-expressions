4ab8af494cee97d80bc9bd6c8bd89f2fde191e73
Jirles <emily.jirles@gmail.com>
2018-09-24 13:40:38 -0400

    Add additional cases to test suite and documentation to function

--- src/functions/COALESCE.ts
+++ src/functions/COALESCE.ts
@@ -2,6 +2,16 @@ import { find } from "lodash"
 import ARRAY from "./ARRAY"
 import ISBLANK from "./ISBLANK"
 
+/**
+ * Returns first value passed in to function that exists
+ * NaN, undefined, null, and empty strings, objects, and arrays are not recognized
+ * @param args array of values
+ * @returns first value passed into function
+ * @example
+ * COALESCE([[2, 3], [3, 6, [5]]]) // returns 2
+ * COALESCE([{}, {}, {foo: "bar"}]) // returns {foo: "bar"}
+ */
+
 export default function COALESCE(...args: any[]): string|number|undefined
 export default function COALESCE(): undefined
 export default function COALESCE(...args: any[]): string|number|undefined {
--- src/functions/__tests__/COALESCE.test.ts
+++ src/functions/__tests__/COALESCE.test.ts
@@ -8,6 +8,7 @@ test("returns the first argument passed in that exists", () => {
   expect(COALESCE([{test: 1}, {stillTest: 2}])).toEqual({test: 1})
   expect(COALESCE({test: 1}, {stillTest: 2})).toEqual({test: 1})
   expect(COALESCE([[2, 3], [3, 6, [5]]])).toEqual(2)
+  expect(COALESCE([{}, {}, {foo: "bar"}])).toEqual({foo: "bar"})
 })
 
 test("returns undefined if arguments passed in are not values", () => {
@@ -15,4 +16,6 @@ test("returns undefined if arguments passed in are not values", () => {
   expect(COALESCE([null, NaN, undefined])).toBeUndefined()
   expect(COALESCE(null, null, NaN)).toBeUndefined()
   expect(COALESCE(NaN)).toBeUndefined()
+  expect(COALESCE([{}, []])).toBeUndefined()
+  expect(COALESCE([])).toBeUndefined()
 })
