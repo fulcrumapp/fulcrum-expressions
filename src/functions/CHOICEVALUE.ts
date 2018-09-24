2e96fb910494443323948d7808f820446daf4d34
Jirles <emily.jirles@gmail.com>
2018-09-24 10:19:52 -0400

    Add documentation to CHOICEVALUE

--- src/functions/CHOICEVALUE.ts
+++ src/functions/CHOICEVALUE.ts
@@ -3,6 +3,14 @@ import { MaybeString } from "../primitives"
 import { ChoiceFieldValue } from "../values"
 import CHOICEVALUES from "./CHOICEVALUES"
 
+/**
+ * Returns the first choice value in the choice field value passed to the function
+ * @param field Choice field value, e.g. `{choice_values: ['a', 'b'], other_values" ['c', 'd']}`
+ * @returns string value, first choice in a list of choice field values
+ * @example
+ * CHOICEVALUE({choice_values: ['a', 'b'], other_values: ['c', 'd']}) // returns 'a'
+ */
+
 export default function CHOICEVALUE(field: ChoiceFieldValue): MaybeString
 export default function CHOICEVALUE(field: any): MaybeString
 export default function CHOICEVALUE(field: any): MaybeString {
