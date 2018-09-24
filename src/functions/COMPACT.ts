ead94b813fd66af516ee2d752cdbfd8b4cb3b742
Jirles <emily.jirles@gmail.com>
2018-09-24 15:38:57 -0400

    Convert COMPACT function and test suite to ts

--- src/functions/COMPACT.ts
+++ src/functions/COMPACT.ts
@@ -1,6 +1,17 @@
 import { filter, isArray } from "lodash"
 
-export default function COMPACT(value: any[]): any[]|undefined {
+/**
+ * Returns a compacted array without null or undefined values.
+ * @param value array of values
+ * @returns a compacted array with null and undefined values removed
+ * @example
+ * COMPACT(['a', 'b', null, 'c']) // returns ['a', 'b', 'c']
+ */
+
+export default function COMPACT(value: any[]): any[]|undefined
+export default function COMPACT(value: any): undefined
+export default function COMPACT(): undefined
+export default function COMPACT(value?: any[]): any[]|undefined {
   if (!isArray(value)) { return undefined }
   // allows falsey value like '', 0, and NaN to pass through
   return filter(value, (item) => item !== null && item !== undefined)
--- src/functions/__tests__/COMPACT.test.ts
+++ src/functions/__tests__/COMPACT.test.ts
@@ -7,10 +7,13 @@ test("it returns a compacted array", () => {
   expect(COMPACT([null])).toEqual([])
   expect(COMPACT([0])).toEqual([0])
   expect(COMPACT(["", 0, 1])).toEqual(["", 0, 1])
+  expect(COMPACT(["a", "b", null, "c", undefined, NaN])).toEqual(["a", "b", "c", NaN])
 })
 
 test("it only accepts an array as an argument", () => {
   expect(COMPACT(null)).toBeUndefined()
   expect(COMPACT(undefined)).toBeUndefined()
   expect(COMPACT({})).toBeUndefined()
+  expect(COMPACT()).toBeUndefined()
+  expect(COMPACT(new Date())).toBeUndefined()
 })
