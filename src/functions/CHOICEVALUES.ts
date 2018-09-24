80c2348aad2e3a95242d92d783a420983942c4f9
Jirles <emily.jirles@gmail.com>
2018-09-24 09:45:26 -0400

    Make tslint happy...

--- src/functions/CHOICEVALUES.ts
+++ src/functions/CHOICEVALUES.ts
@@ -1,12 +1,11 @@
-import ISBLANK from './ISBLANK';
-import { isUndefined, compact, concat } from 'lodash';
-import { FormFieldValues as ChoiceFieldValue} from '../values';
+import { compact, concat, isUndefined } from "lodash"
+import { FormFieldValues as ChoiceFieldValue} from "../values"
+import ISBLANK from "./ISBLANK"
 
 /**
  * Converts a choicefield object to an array with null values and blank strings removed
  * @param field ChoiceFieldValue: {choice_values: [ ... ], other_values: [ ... ]}
  * @returns array
- * 
  * @example
  * CHOICEVALUES({choice_values: ['a', 'b'], other_values: ['c', 'd']}) // returns [ 'a', 'b', 'c', 'd' ]
  */
@@ -14,10 +13,10 @@ export default function CHOICEVALUES(field: ChoiceFieldValue): string[]
 export default function CHOICEVALUES(field: any): any[]
 export default function CHOICEVALUES(): undefined
 export default function CHOICEVALUES(field?: any): any[]|undefined {
-  if(isUndefined(field)) return undefined;
-  if (ISBLANK(field)) return [];
+  if (isUndefined(field)) {return undefined}
+  if (ISBLANK(field)) {return []}
 
-  const values: any[] = concat(field.choice_values, field.other_values);
+  const values: any[] = concat(field.choice_values, field.other_values)
 
   return compact(values)
 }
