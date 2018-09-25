8550f4f504d6ce52854ebbec14ccfcfe27bf483d
Tyler Johnston <tylerjohnst@gmail.com>
2018-09-25 09:59:13 -0400

    Fix jsdoc ordering

--- src/functions/CLEAN.ts
+++ src/functions/CLEAN.ts
@@ -1,3 +1,6 @@
+/** The invisible character codes */
+const CLEAN_REGEX = /[\x00\x08\x0B\x0C\x0E-\x1F]/g
+
 /**
  * Removes non-printable characters from a string
  * @param text string to be cleaned
@@ -5,8 +8,6 @@
  * @example
  * CLEAN('test\x00\x1D\x1Etest') // returns 'testtest'
  */
-
-const CLEAN_REGEX = /[\x00\x08\x0B\x0C\x0E-\x1F]/g
 
 export default function CLEAN(text: string): string {
   return text.replace(CLEAN_REGEX, "")
