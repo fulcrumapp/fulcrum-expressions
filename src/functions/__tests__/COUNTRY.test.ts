213dd1eb818494bea521c6b4de695e5407b8a1e1
Jirles <emily.jirles@gmail.com>
2018-09-28 11:37:52 -0400

    Add a some comments and new check to COUNTRY test suite

--- src/functions/__tests__/COUNTRY.test.ts
+++ src/functions/__tests__/COUNTRY.test.ts
@@ -5,6 +5,7 @@ import COUNTRY from "../COUNTRY"
 beforeEach(RESETCONFIG)
 
 test("it returns the country from the configuration object", () => {
+  // default set to US
   expect(COUNTRY()).toBe("US")
 
   CONFIGURE({country: "DE"})
@@ -13,7 +14,10 @@ test("it returns the country from the configuration object", () => {
 
 test("it returns the default country if country is not present on configuration obejct", () => {
   CONFIGURE({country: "DE"})
+  expect(COUNTRY()).toBe("DE")
 
+  // passing in false overwrites the entire config object so the key
+  // currencyCode no longer exists
   CONFIGURE({ recordAltitude: 456 }, false)
   expect(COUNTRY()).toBe("US")
 })
