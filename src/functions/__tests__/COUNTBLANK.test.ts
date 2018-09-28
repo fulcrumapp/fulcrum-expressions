bf0d9e856d81e26cde34b2db70c88f0289ce2901
Jirles <emily.jirles@gmail.com>
2018-09-28 10:16:18 -0400

    Convert COUNTBLANK Test suite to ts

---
+++ src/functions/__tests__/COUNTBLANK.test.ts
@@ -0,0 +1,13 @@
+import COUNTBLANK from "../COUNTBLANK"
+
+test("it returns a count of blank items", () => {
+  expect(COUNTBLANK([1, 2, 3])).toBe(0)
+  expect(COUNTBLANK([1, 2, ""])).toBe(1)
+  expect(COUNTBLANK([1, 2, "", undefined])).toBe(2)
+  expect(COUNTBLANK([])).toBe(0)
+  expect(COUNTBLANK({})).toBe(0)
+  expect(COUNTBLANK([undefined])).toBe(1)
+  expect(COUNTBLANK(null)).toBe(1)
+  expect(COUNTBLANK(0)).toBe(0)
+  expect(COUNTBLANK(1, 3, null)).toBe(1)
+})
