58c052b3f8a887005ef723acf57929262ca994db
Jirles <emily.jirles@gmail.com>
2018-09-28 15:03:09 -0400

    Convert FLOOR test suite to ts

---
+++ src/functions/__tests__/FLOOR.test.ts
@@ -0,0 +1,25 @@
+import FLOOR from "../FLOOR"
+
+test("returns number rounded down, towards zero, to the nearest multiple", () => {
+    expect(FLOOR(1)).toBe(1)
+    expect(FLOOR(1.5)).toBe(1)
+    expect(FLOOR(2.5)).toBe(2)
+    expect(FLOOR(-45)).toBe(-45)
+    expect(FLOOR(2.3333333, 4)).toBe(0)
+    expect(FLOOR(0.13, 0.25)).toBe(0)
+    expect(FLOOR(0.31, 0.25)).toBe(0.25)
+    expect(FLOOR(-0.13, 0.25)).toBe(-0.25)
+    expect(FLOOR(-0.31, 0.25)).toBe(-0.5)
+})
+
+test("it accepts a number as a string", () => {
+  expect(FLOOR("2.5")).toBe(2)
+  expect(FLOOR("-34.6")).toBe(-35)
+})
+
+test("it returns NaN if an invalid input is passed", () => {
+  expect(FLOOR("test")).toBeNaN()
+  expect(FLOOR()).toBeNaN()
+  // @ts-ignore Bad input on purpose
+  expect(FLOOR(true)).toBeNaN()
+})
