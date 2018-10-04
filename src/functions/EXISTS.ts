fc5c7071ec8fb67303b596e373f1ea4a701e8f54
Jirles <emily.jirles@gmail.com>
2018-10-04 13:27:04 -0400

    Convert EXISTS to ts

---
+++ src/functions/EXISTS.ts
@@ -0,0 +1,18 @@
+import { filter, toArray } from "lodash"
+import ISBLANK from "./ISBLANK"
+
+/**
+ * Checks whether all values passed in exist.
+ * @param args list of items to check
+ * @returns boolean value indicating whether all values exist
+ * @example
+ * EXISTS(1, 3, 7, 0) // returns true
+ * EXISTS(1, null, 7, 0) // returns false
+ */
+
+export default function EXISTS(...args: any[]): boolean {
+  const valuesAreBlank: boolean[] = toArray(args).map(ISBLANK)
+  const totalBlankValues: boolean[] = filter(valuesAreBlank)
+
+  return totalBlankValues.length === 0
+}
