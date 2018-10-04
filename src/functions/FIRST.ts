32967b81d87a578c30a5fd2d8dab4d072d8d5644
Jirles <emily.jirles@gmail.com>
2018-10-04 17:06:21 -0400

    Remove debugger

--- src/functions/FIRST.ts
+++ src/functions/FIRST.ts
@@ -8,7 +8,6 @@ export default function FIRST(item: any[]|string, n: number): any[]|undefined
 export default function FIRST(item: any[]|string, n?: number): any[]|undefined
 export default function FIRST(item: any): any[]|undefined
 export default function FIRST(item: any[]|string, n = 0): any[]|undefined {
-  debugger
   if (!isArray(item) && !isString(item)) { return undefined }
   if (isEmpty(item)) { return undefined }
   if (isString(item) ) { item = item.split("") }
