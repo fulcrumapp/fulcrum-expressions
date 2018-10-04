0cb42fbe264ec5b6c91e2d663ed31926f0f66e38
Jirles <emily.jirles@gmail.com>
2018-10-04 12:42:25 -0400

    Convert EVEN to ts

---
+++ src/functions/EVEN.ts
@@ -0,0 +1,23 @@
+import { isNumber } from "lodash"
+import CEILING from "./CEILING"
+import NUM from "./NUM"
+
+/**
+ * Returns number rounded up to the nearest even integer
+ * @param value number
+ * @returns number or NaN
+ * @example
+ * EVEN(-3.4) // returns -2
+ * EVEN([]) // returns NaN
+ */
+
+export default function EVEN(value: number): number
+export default function EVEN(value: string): number
+export default function EVEN(value: any): number
+export default function EVEN(value: any): number {
+  value = NUM(value)
+
+  if (!isNumber(value)) { return NaN }
+
+  return CEILING(value, -2)
+}
