72baaa83794dbcdd354e9b5396565933cfece81b
Jirles <emily.jirles@gmail.com>
2018-10-04 17:05:01 -0400

    Convert FIRST to ts

---
+++ src/functions/FIRST.ts
@@ -0,0 +1,21 @@
+import { isArray, isEmpty, isString, slice } from "lodash"
+
+/**
+ * Returns the first n items of an array or string.
+ */
+
+export default function FIRST(item: any[]|string, n: number): any[]|undefined
+export default function FIRST(item: any[]|string, n?: number): any[]|undefined
+export default function FIRST(item: any): any[]|undefined
+export default function FIRST(item: any[]|string, n = 0): any[]|undefined {
+  debugger
+  if (!isArray(item) && !isString(item)) { return undefined }
+  if (isEmpty(item)) { return undefined }
+  if (isString(item) ) { item = item.split("") }
+
+  if (item.length === 1 || n === 0) {
+    return slice(item, 0)[0]
+  } else {
+  return slice(item, 0, n)
+  }
+}
