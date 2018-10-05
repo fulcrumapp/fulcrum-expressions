6de1740b0c18eeef18fd6eb06fe6036300d88781
Jirles <emily.jirles@gmail.com>
2018-10-05 16:19:05 -0400

    Convert FACT to ts

---
+++ src/functions/FACT.ts
@@ -0,0 +1,30 @@
+import {isNaN} from "lodash"
+import MEMOIZED_FACT from "./MEMOIZED_FACT"
+import NUM from "./NUM"
+
+/**
+ * Returns factorial of a number, n (n!)
+ * @param value postive integer
+ * @returns factorial of value (value!)
+ * @example
+ * FACT(0) // returns 1
+ * FACT(7) // returns 5040
+ */
+
+export default function FACT(value: any): number {
+  value = NUM(value)
+
+  if (isNaN(value) || value < 0) { return NaN }
+
+  const n: number = Math.floor(value)
+
+  if (n === 0 || n === 1) {
+    return 1
+  } else if (MEMOIZED_FACT()[n] > 0) {
+    return MEMOIZED_FACT()[n]
+  } else {
+    return MEMOIZED_FACT()[n] = FACT(n - 1) * n
+  }
+
+  return MEMOIZED_FACT()[n]
+}
