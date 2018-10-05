8a8c2eb83f050ef92cc24d784acce6e3ff78fe2b
Jirles <emily.jirles@gmail.com>
2018-10-05 16:19:48 -0400

    Add FACT test suite

---
+++ src/functions/__tests__/FACT.test.ts
@@ -0,0 +1,25 @@
+import FACT from "../FACT"
+import { RESET_MEMOIZED_FACT} from "../MEMOIZED_FACT"
+
+beforeEach(RESET_MEMOIZED_FACT)
+
+test("returns the factorial of a postive integers", () => {
+  expect(FACT(0)).toBe(1)
+  expect(FACT(1)).toBe(1)
+  expect(FACT(2)).toBe(2)
+  expect(FACT(3)).toBe(6)
+  expect(FACT(4)).toBe(24)
+  expect(FACT(7)).toBe(5040)
+  expect(FACT(8)).toBe(40320)
+})
+
+test("returns NaN for invalid input", () => {
+  expect(FACT(-1)).toBeNaN()
+  expect(FACT(NaN)).toBeNaN()
+  expect(FACT(true)).toBeNaN()
+  expect(FACT([])).toBeNaN()
+  expect(FACT({})).toBeNaN()
+  expect(FACT("")).toBeNaN()
+  expect(FACT(null)).toBeNaN()
+  expect(FACT(undefined)).toBeNaN()
+})
