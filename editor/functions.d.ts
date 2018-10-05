08222aac57b22584777de610ab6748c1fe60db60
Tyler Johnston <tylerjohnst@gmail.com>
2018-10-05 15:16:12 -0400

    Tabs => Spaces

--- editor/functions.d.ts
+++ editor/functions.d.ts
@@ -1976,6 +1976,39 @@ declare function FIELDS(dataName: string, options: FieldsOptions): FormFields[];
 declare function FIELDS(): undefined;
 
 /**
+ * Returns child field names when passed in a parent's dataname
+ * @param dataName required; string
+ * @param options optional; object indicating if result should be flattened recursively,
+ * e.g. `{ repeatables: boolean, sections: boolean }`
+ * @returns array of child field names
+ * @example
+ * FIELDNAMES('items')
+ * // returns ['cost', 'choice_value', 'child_items', 'child_item_cost']
+ *
+ * // do not recursively look for child records of repeatable children
+ * FIELDNAMES('items', { repeatables: false })
+ * // returns ['cost', 'choice_value', 'child_items']
+ */
+declare function FIELDNAMES(dataName: string, options?: FieldsOptions): string[] | undefined;
+/**
+ * Returns child field names when passed in a parent's dataname
+ * @param dataName required; string
+ * @param options optional; object indicating if result should be flattened recursively,
+ * e.g. `{ repeatables: boolean, sections: boolean }`
+ * @returns array of child field names
+ * @example
+ * FIELDNAMES('items')
+ * // returns ['cost', 'choice_value', 'child_items', 'child_item_cost']
+ *
+ * // do not recursively look for child records of repeatable children
+ * FIELDNAMES('items', { repeatables: false })
+ * // returns ['cost', 'choice_value', 'child_items']
+ */
+declare function FIELDNAMES(dataName: string): string[] | undefined;
+
+declare function FIELDTYPE(dataName: string): string | undefined;
+
+/**
  * Returns the first n items of an array or string.
  */
 declare function FIRST(item: any[] | string, n: number): any[] | undefined;
