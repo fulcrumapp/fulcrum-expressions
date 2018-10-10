0573a74b878160e10c71611facd5fe0412645181
Jirles <emily.jirles@gmail.com>
2018-10-10 09:14:20 -0400

    Add AddressFieldValue to values file

--- src/types/values.d.ts
+++ src/types/values.d.ts
@@ -1,5 +1,18 @@
 export type FormFieldValues =
   ChoiceFieldValue
+  | AddressFieldValue
+
+
+export interface AddressFieldValue {
+  sub_thoroughfare?: string,
+  thoroughfare?: string,
+  suite?: string,
+  locality?: string,
+  sub_admin_area?: string, 
+  admin_area?: string, 
+  postal_code?: string,
+  country?: string
+}
 
 export interface ChoiceFieldValue {
   choice_values?: string[] | null,
