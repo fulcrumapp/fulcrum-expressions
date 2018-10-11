6cfba8c9cd5e9e4374ad1b780710df3986df8c1d
Jirles <emily.jirles@gmail.com>
2018-10-11 18:33:59 -0500

    Convert ISLANDSCAPE to ts

---
+++ src/functions/ISLANDSCAPE.ts
@@ -0,0 +1,20 @@
+import { isUndefined } from "lodash"
+import ISPORTRAIT, { MediaObject } from "./ISPORTRAIT"
+
+/**
+ * Checks whether the media is in landscape mode.
+ * @param media required; media object, height and width attributes must be present
+ * @returns boolean
+ * @example
+ * const mediaObject = { width: 200, height: 100 }
+ * ISLANDSCAPE(mediaObject) // returns true
+ */
+
+export default function ISLANDSCAPE(media: MediaObject): boolean
+export default function ISLANDSCAPE(media: any): boolean|undefined
+export default function ISLANDSCAPE(): undefined
+export default function ISLANDSCAPE(media?: MediaObject|any): boolean|undefined {
+  if (isUndefined(media) || isUndefined(media.width) || isUndefined(media.height) ) { return undefined }
+
+  return !ISPORTRAIT(media)
+}
