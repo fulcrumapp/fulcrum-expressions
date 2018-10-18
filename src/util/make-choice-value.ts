a7e7799f52c6d99bae3722491163e5b3fcd3efff
Jirles <emily.jirles@gmail.com>
2018-10-18 16:02:45 -0400

    Add ChoiceFieldValue return type to makeChoiceValue util function

--- src/util/make-choice-value.ts
+++ src/util/make-choice-value.ts
@@ -1,6 +1,7 @@
 import EXISTS from "../functions/EXISTS"
+import { ChoiceFieldValue } from "../types/values"
 
-export default function makeChoiceValue(choices: string[], others: string[]): {} {
+export default function makeChoiceValue(choices: string[], others: string[]): ChoiceFieldValue {
   choices = EXISTS(choices) ? choices : []
   others = EXISTS(others) ? others : []
   return { choice_values: choices, other_values: others }
