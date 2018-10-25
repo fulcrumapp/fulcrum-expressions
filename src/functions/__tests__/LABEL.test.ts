2471b5b9991adf804505cd74fbc9b13168e87f4f
Jirles <emily.jirles@gmail.com>
2018-10-25 17:09:29 -0400

    Convert LABEL test to ts

---
+++ src/functions/__tests__/LABEL.test.ts
@@ -0,0 +1,14 @@
+import form from "../../test/fixtures/form"
+import { prepareRuntime } from "../../test/helpers"
+import LABEL from "../LABEL"
+
+beforeEach(() => {
+  prepareRuntime()
+  // @ts-ignore definitely a text field even if ts-lint says it isn't
+  $$runtime.elementsByDataName.name = form.elements[0]
+})
+
+test("returns the label of a field", () => {
+  expect(LABEL("name")).toEqual("Name")
+  expect(LABEL("invalid_field")).toBeUndefined()
+})
