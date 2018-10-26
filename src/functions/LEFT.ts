ee724a90675c8515a5446e6b87e2aa83cad2284e
Jirles <emily.jirles@gmail.com>
2018-10-26 12:19:00 -0400

    Add documentation to LEFT

--- src/functions/LEFT.ts
+++ src/functions/LEFT.ts
@@ -2,6 +2,16 @@ import { isNull, isObject, isUndefined } from "lodash"
 import FLOOR from "./FLOOR"
 import ISNAN from "./ISNAN"
 
+/**
+ * Returns n left characters of a string.
+ * @param value required; string
+ * @param numberOfCharacters optional; number of characters to be returned. If not specified
+ * one character will be returned
+ * @returns string; number of characters specified
+ * @example
+ * LEFT("Hello, World", 3) // returns "Hel"
+ */
+
 export default function LEFT(value: string, numberOfCharacters?: number): string|undefined
 export default function LEFT(value: any, numberOfCharacters?: string): string|undefined
 export default function LEFT(value: string): string|undefined
