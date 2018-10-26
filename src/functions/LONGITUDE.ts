4268fc7e5754cc7cf39d91ce216b29210617c7c2
Jirles <emily.jirles@gmail.com>
2018-10-26 10:23:23 -0400

    Add documentation to LONGITUDE

--- src/functions/LONGITUDE.ts
+++ src/functions/LONGITUDE.ts
@@ -2,6 +2,10 @@ import { isUndefined } from "lodash"
 import { CONFIG, FeatureGeometry } from "./CONFIG"
 import NUM from "./NUM"
 
+/**
+ * Returns a record's longitude if it exists.
+ */
+
 export default function LONGITUDE(): number {
   const featureGeometry: FeatureGeometry|undefined = CONFIG().featureGeometry
   if (isUndefined(featureGeometry)) { return NaN }
