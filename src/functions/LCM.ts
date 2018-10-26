1ded1d7ce0e913d8deb4315a25e2212df5e7957d
Jirles <emily.jirles@gmail.com>
2018-10-26 11:27:16 -0400

    Convert LCM to ts, add documentation

---
+++ src/functions/LCM.ts
@@ -0,0 +1,38 @@
+import ARRAY from "./ARRAY"
+import INT from "./INT"
+import ISNAN from "./ISNAN"
+
+/**
+ * Returns the least common multiple of the arguments passed in
+ * @param args numbers or numbers as string values to be evaluated
+ * @returns number; least common multiple
+ * @example
+ * LCM(-50, 25, -45, -18, 90, 447) // returns 67050
+ */
+
+export default function LCM(...args: number[]): number
+export default function LCM(...args: string[]): number
+export default function LCM(...args: any[]): number {
+  const numbers: number[] = ARRAY((args)).map((num) => INT(num))
+
+  const count: number = numbers.length
+
+  let a: number = Math.abs(numbers[0])
+
+  for (let i: number = 1; i < count; i++) {
+    let b: number = Math.abs(numbers[i])
+    const c: number = a
+
+    if (ISNAN(b)) { return NaN }
+
+    while (a && b) {
+      if (a > b) {
+        a %= b
+      } else {
+        b %= a
+      }
+    }
+    a = Math.abs(c * numbers[i]) / (a + b)
+  }
+  return a
+}
