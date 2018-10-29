98d9160ed91b1186091ee279b6101f5b85930ff7
Jirles <emily.jirles@gmail.com>
2018-10-29 17:02:51 -0400

    Add documentation to MOD

--- src/functions/MOD.ts
+++ src/functions/MOD.ts
@@ -1,6 +1,16 @@
 import { isNaN } from "lodash"
 import NUM from "./NUM"
 
+/**
+ * Returns the modulus or remainder of a number divided by a divisor.
+ * @param num required; number to be divided
+ * @param divisor required; number doing the dividing
+ * @returns numeric value; remainder of `num / divisor`
+ * @example
+ * MOD(10, 2) // returns 0
+ * MOD(13, 2) // returns 1
+ */
+
 export default function MOD(num: number, divisor: number): number
 export default function MOD(num: string, divisor: string): number
 export default function MOD(num?: any, divisor?: any): number
