907b840e37df7b680e27d4a44ca59ee379dc1fac
Jirles <emily.jirles@gmail.com>
2018-10-29 18:01:53 -0400

    Convert N to ts

---
+++ src/functions/N.ts
@@ -0,0 +1,19 @@
+import { isNumber } from "lodash"
+
+/**
+ * Returns a numeric value. If a number if passed in, the same number is returned, otherwise
+ * function returns 1 for a true boolean value, and 0 for all other values.
+ * @param value optional
+ * @returns a numeric value
+ * @example
+ * N(97) // returns 97
+ * N(false) // returns 0
+ */
+
+export default function N(value: any): number {
+  if (isNumber(value)) { return value }
+  if (value === true) { return 1 }
+  if (value === false) { return 0 }
+
+  return 0
+}
