e1ce8bfb9f7c379ead95209acc333588556c826e
Jirles <emily.jirles@gmail.com>
2018-10-31 14:53:10 -0400

    Add documentation to NOT

--- src/functions/NOT.ts
+++ src/functions/NOT.ts
@@ -1,3 +1,11 @@
+/**
+ * Returns the negation of a value, i.e. if a value is falsey NOT() will return true.
+ * @param value parameter of any type
+ * @returns boolean
+ * @example
+ * NOT("test") // returns false
+ */
+
 export default function NOT(value?: any): boolean {
   return value ? false : !value
 }
