bd95cd0bbbb257a2024f13a8608c06f5b3489ad8
Jirles <emily.jirles@gmail.com>
2018-10-31 16:42:52 -0400

    Change documentation

--- src/functions/PLATFORMVERSION.ts
+++ src/functions/PLATFORMVERSION.ts
@@ -1,6 +1,6 @@
 import { CONFIG } from "./CONFIG"
 
-/** Returns the platform name from the configuration object */
+/** Returns the platform version from the configuration object */
 
 export default function PLATFORMVERSION(): string {
   return CONFIG().platformVersion || ""
