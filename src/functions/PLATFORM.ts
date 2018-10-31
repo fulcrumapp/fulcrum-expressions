3ec6355ee4e1d423eda4eb29ca4838122750e63f
Jirles <emily.jirles@gmail.com>
2018-10-31 16:22:00 -0400

    Add documentation to PLATFORM

--- src/functions/PLATFORM.ts
+++ src/functions/PLATFORM.ts
@@ -1,5 +1,7 @@
 import { CONFIG } from "./CONFIG"
 
+/** Returns the platform name from the configuration object */
+
 export default function PLATFORM(): string {
   return CONFIG().platform || ""
 }
