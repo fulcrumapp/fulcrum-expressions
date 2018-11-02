b977718ebd93de4b22fdc7e2f02c599202f5776c
Jirles <emily.jirles@gmail.com>
2018-11-02 13:55:18 -0400

    Add documentation, change param organization in PROGRESS

--- src/functions/PROGRESS.ts
+++ src/functions/PROGRESS.ts
@@ -12,18 +12,30 @@ export interface ProgressResult {
 }
 
 /**
- * Something about progress.
+ * Displays a non-dismissible message that can be used to provide feedback when performing an asynchronous function.
+ * @description While fetching data from an API using REQUEST it might be desirable
+ * to let the user know that the request is in progress. This is an advanced function that requires thorough
+ * testing and error checking in your logic since the message is not dismissible by the user. To dismiss
+ * the progress message, call PROGRESS();.
+ * @param title required; A short title for the progress message
+ * @param message required; The message content for the progress alert
+ * @example
+ * const url: string = 'https://example.com';
+ * // show progress message while request is happening
+ * PROGRESS('Searching for nearby facilities ...');
+ *
+ * REQUEST(url, function(error, response, body) {
+ *  // resets progress message after request finishes
+ *  PROGRESS();
+ *  if (error) {
+ *    ALERT(INSPECT(error));
+ *  } else {
+ *   // do something with the API response
+ *  }
+ * });
  */
 
-export default function PROGRESS(...args: string[]): void {
-  let title: string|null = null
-  let message: string = args[0]
-
-  if (args.length > 1) {
-    title = arguments[0]
-    message = arguments[1]
-  }
-
+export default function PROGRESS(title?: null|string, message?: null|string): void {
   const result: ProgressResult = {
     message: isNull(message) || isUndefined(message) ? null : message.toString(),
     title: isNull(title) || isUndefined(title) ? null : title.toString(),
