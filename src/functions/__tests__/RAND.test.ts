daf5d33e7069beb96838069fd2f19acab3fcb60d
Jirles <emily.jirles@gmail.com>
2018-11-05 12:34:45 -0500

    Remove hard coded toBeWithinRange extension, import from helpers

--- src/functions/__tests__/RAND.test.ts
+++ src/functions/__tests__/RAND.test.ts
@@ -1,23 +1,7 @@
+import { extendToBeWithinRange } from "../../test/helpers"
 import RAND from "../RAND"
 
-expect.extend({
-  toBeWithinRange(received, floor, ceiling) {
-    const pass: boolean = received >= floor && received <= ceiling
-    if (pass) {
-      return {
-        message: () =>
-          `expected ${received} not to be within range ${floor} - ${ceiling}`,
-        pass: true,
-      }
-    } else {
-      return {
-        message: () =>
-          `expected ${received} to be within range ${floor} - ${ceiling}`,
-        pass: false,
-      }
-    }
-  },
-})
+beforeEach(extendToBeWithinRange)
 
 test("returns a random number between 0 and 1", () => {
   // @ts-ignore Extended expect to include toBeWithinRange matcher
