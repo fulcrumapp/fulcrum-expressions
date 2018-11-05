a9e676c613f53221e1c3809d22fef970a3aa672b
Jirles <emily.jirles@gmail.com>
2018-11-05 12:33:06 -0500

    Add extendToBeWithinRange jest matcher to helpers

--- src/test/helpers.ts
+++ src/test/helpers.ts
@@ -10,3 +10,22 @@ export const finishAsync = (callbackID: number) => {
   $$runtime.callbackID = callbackID
   $$runtime.finishAsync()
 }
+
+export const extendToBeWithinRange = () => expect.extend({
+  toBeWithinRange(received, floor, ceiling) {
+    const pass: boolean = received >= floor && received <= ceiling
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} not to be within range ${floor} - ${ceiling}`,
+        pass: true,
+      }
+    } else {
+      return {
+        message: () =>
+          `expected ${received} to be within range ${floor} - ${ceiling}`,
+        pass: false,
+      }
+    }
+  },
+})
