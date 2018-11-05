9ba60af55b95f2d281206aaf59a1a2dcfd264b4b
Jirles <emily.jirles@gmail.com>
2018-11-05 12:12:07 -0500

    Convert RAND to ts

---
+++ src/functions/RAND.ts
@@ -0,0 +1,10 @@
+/**
+ * Returns a random number between 0 and 1.
+ * @example
+ * RAND() // returns 0.45
+ * RAND() // returns 0.91
+ */
+
+export default function RAND(): number {
+  return Math.random()
+}
