bec9e7f6499a26f2c2cbf4f9d8f5f18fc02d661e
Jirles <emily.jirles@gmail.com>
2018-11-07 10:41:46 -0500

    Expand isNumericElement test suite to prove numeric field check

--- src/util/__tests__/is-numeric-element.test.ts
+++ src/util/__tests__/is-numeric-element.test.ts
@@ -4,6 +4,7 @@ import isNumericElement from "../is-numeric-element"
 test("tests to see if an element is numeric", () => {
   const numericElement: CalculatedField = {
     type: "CalculatedField",
+    // tslint:disable-next-line:object-literal-sort-keys
     display: { style: "currency", currency: "us-EN" },
     key: "8ui0",
     label: "My Calculated Field",
@@ -13,10 +14,25 @@ test("tests to see if an element is numeric", () => {
     hidden: false,
   }
 
+  const alsoNumeric: FormTextField = {
+    type: "TextField",
+    // tslint:disable-next-line:object-literal-sort-keys
+    key: "8ui0",
+    numeric: true,
+    format: "decimal",
+    label: "My Number Text Field",
+    data_name: "my_number_text_field",
+    required: false,
+    disabled: false,
+    hidden: false,
+  }
+
   expect(isNumericElement(numericElement)).toBe(true)
+  expect(isNumericElement(alsoNumeric)).toBe(true)
 
   const notNumericElement: FormTextField = {
     type: "TextField",
+    // tslint:disable-next-line:object-literal-sort-keys
     key: "9oi3",
     label: "Name",
     data_name: "name",
