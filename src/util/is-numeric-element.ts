25d6b879115d7b587e9f0a1a325bb937d4856bb5
Jirles <emily.jirles@gmail.com>
2018-11-07 10:40:57 -0500

    Add ts-ignores because not all form fields have certain fields

--- src/util/is-numeric-element.ts
+++ src/util/is-numeric-element.ts
@@ -8,8 +8,11 @@ import { FormFields } from "../types/fields"
  */
 
 export default function isNumericElement(element: FormFields) {
+          // @ts-ignore Numeric field does not exist on all FormField types
   return (element.numeric ||
+          // @ts-ignore Display field does not exist on all FormField types
           (!isUndefined(element.display) &&
+          // @ts-ignore Display field does not exist on all FormField types
           (element.display.style === "number" || element.display.style === "currency"))
         )
 }
