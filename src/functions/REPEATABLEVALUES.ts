420c0b9d676c3fc5e5b89ad0abb8d9f48bae68ba
Jirles <emily.jirles@gmail.com>
2018-11-08 17:56:12 -0500

    Convert REPEATABLEVALUES to ts

---
+++ src/functions/REPEATABLEVALUES.ts
@@ -0,0 +1,34 @@
+import { flatten, isArray, isNull, isUndefined } from "lodash"
+import { FormFields } from "../types/fields"
+import nearestRepeatable from "../util/nearest-repeatable"
+import repeatableValues from "../util/repeatable-values"
+
+/**
+ * Returns a specific field out of a collection of repeatable items.
+ */
+
+export default function REPEATABLEVALUES(repeatableValue: RepeatableField, dataName: string[]|string): any[]|undefined {
+  if (isArray(dataName)) {
+    if (dataName.length === 1) {
+      dataName = dataName[0]
+    } else {
+      const repeatableDataName: string = dataName[0]
+      const restOfDataNames: string[] = dataName.slice(1)
+
+      const childValues: any[]|null = REPEATABLEVALUES(repeatableValue, repeatableDataName).map((item) =>
+      REPEATABLEVALUES(item, restOfDataNames))
+
+      return flatten(childValues)
+    }
+  }
+
+  const dataElement: FormFields = $$runtime.elementsByDataName[dataName]
+
+  if (isUndefined(dataElement) || isNull(dataElement)) { return }
+
+  const repeatableElement = nearestRepeatable(dataElement)
+
+  if (isUndefined(repeatableElement) || isNull(repeatableElement)) { return }
+
+  return repeatableValues(repeatableElement, repeatableValue, dataName)
+}
