a3fd2f81e31bfa11c2d108c887389d059317e910
Jirles <emily.jirles@gmail.com>
2018-11-08 14:02:30 -0500

    Add documentation to repeatableValues

--- src/util/repeatable-values.ts
+++ src/util/repeatable-values.ts
@@ -3,7 +3,13 @@ import { FormFields, RepeatableField } from "../types/fields"
 import repeatableValueElements from "./repeatable-value-elements"
 import valueForElement from "./value-for-element"
 
-// items: [ { form_values: { '89hu': "test"} }, { form_values: { '76tv': 9876 } } ]
+/**
+ * Returns an array of values for a specific field contained in a repeatable.
+ * @param repeatable required; the repeatable field containing the desired field
+ * @param items required; array of objects in this format: { form_values: { form_key: value } }
+ * @param dataName required; data name of desired field
+ * @returns array of values
+ */
 
 export default function repeatableValues(repeatable: RepeatableField,
                                          items: any[],
