ae8cbc2f8a273373fd10aadbaa899987fb041b24
Jirles <emily.jirles@gmail.com>
2018-11-12 11:11:32 -0500

    Add form_values to test fixtures

---
+++ src/test/fixtures/form_values.ts
@@ -0,0 +1,68 @@
+export default {
+  form_values: {
+    "97ab": "Test Record",
+    // tslint:disable-next-line:object-literal-sort-keys
+    "1337": [
+      {
+        id: "1",
+        // tslint:disable-next-line:object-literal-sort-keys
+        form_values: {
+          "1338": 1,
+          "362a": {
+            choice_values: [ "widget" ],
+            other_values: [],
+          },
+          // tslint:disable-next-line:object-literal-sort-keys
+          "1347": [
+            {
+              id: "1a",
+              // tslint:disable-next-line:object-literal-sort-keys
+              form_values: {
+                1399: 4,
+              },
+            },
+            {
+              id: "1b",
+              // tslint:disable-next-line:object-literal-sort-keys
+              form_values: {
+                1399: 5,
+              },
+            },
+          ],
+        },
+      },
+      {
+        id: "2",
+        // tslint:disable-next-line:object-literal-sort-keys
+        form_values: {
+          "1338": 2,
+          "362a": {
+            choice_values: [ "spinner" ],
+            other_values: [],
+          },
+          // tslint:disable-next-line:object-literal-sort-keys
+          "1347": [
+            {
+              id: "2a",
+              // tslint:disable-next-line:object-literal-sort-keys
+              form_values: {
+                1399: 10,
+              },
+            },
+          ],
+        },
+      },
+      {
+        id: "3",
+        // tslint:disable-next-line:object-literal-sort-keys
+        form_values: {
+          "1338": 3,
+          "362a": {
+            choice_values: [ "gizmo" ],
+            other_values: [],
+          },
+        },
+      },
+    ],
+  },
+}
