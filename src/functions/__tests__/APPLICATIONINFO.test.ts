771a2210efd688f6cca93bd1c0025bf406fb9eb6
Jirles <emily.jirles@gmail.com>
2018-11-20 12:12:30 -0500

    Add test for separator parameter in APPLICATIONINFO.test

--- src/functions/__tests__/APPLICATIONINFO.test.ts
+++ src/functions/__tests__/APPLICATIONINFO.test.ts
@@ -14,3 +14,11 @@ test("fetches name of application engine, application platform, and application 
 test("falls back to an empty string", () => {
   expect(APPLICATIONINFO()).toEqual("")
 })
+
+test("accepts a separator parameter for formatting", () => {
+  CONFIG().application = "Chrome"
+  CONFIG().applicationVersion = "4.2.3.5.2"
+  CONFIG().applicationBuild = "Webkit"
+
+  expect(APPLICATIONINFO(": ")).toEqual("Chrome: 4.2.3.5.2: Webkit")
+})
