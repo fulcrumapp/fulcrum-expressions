2fe51aa4a8fe6c8501c4fbb5e76ebd13efcd1d43
Jirles <emily.jirles@gmail.com>
2018-11-20 12:10:36 -0500

    Add separator parameter to APPLICATIONINFO, edit documentation to reflect change

--- src/functions/APPLICATIONINFO.ts
+++ src/functions/APPLICATIONINFO.ts
@@ -6,11 +6,13 @@ import APPLICATIONVERSION from "./APPLICATIONVERSION"
 /**
  * Returns the name of the application engine, application platform, and application version
  * on which Fulcrum is running
+ * @param separator optional; character string to separate application information, defaults to ", "
  * @returns name of application engine, application platform, and application version.
  * @example
  * // running Fulcrum Desktop on Chrome Browser
  * APPLICATIONINFO() // returns 'Chrome, 68.43.9.0.1, WebKit'
  */
-export default function APPLICATIONINFO() {
-  return compact([APPLICATION(), APPLICATIONVERSION(), APPLICATIONBUILD()]).join(", ")
+export default function APPLICATIONINFO(separator?: string): string
+export default function APPLICATIONINFO(separator = ", "): string {
+  return compact([APPLICATION(), APPLICATIONVERSION(), APPLICATIONBUILD()]).join(separator)
 }
