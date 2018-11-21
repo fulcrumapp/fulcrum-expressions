2b56cf68e985f70e4933263bdb2ff039cf437e62
Jirles <emily.jirles@gmail.com>
2018-11-21 12:47:00 -0500

    Add documentation to DESCRIPTION

--- src/functions/DESCRIPTION.ts
+++ src/functions/DESCRIPTION.ts
@@ -3,6 +3,14 @@ import { FormFields as FormField } from "../types/fields"
 import { MaybeString } from "../types/primitives"
 import FIELD from "./FIELD"
 
+/**
+ * Returns a field's description.
+ * @param dataName required; data_name of desired field
+ * @returns the field's description
+ * @example
+ * DESCRIPTION("operating_hours") // returns "Stores operating hours in 24-hour time format"
+ */
+
 export default function DESCRIPTION(dataName: MaybeString): string | undefined
 export default function DESCRIPTION(): undefined
 export default function DESCRIPTION(dataName?: MaybeString): string | undefined {
