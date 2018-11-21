0944355a91b48e1abb04630e2f9641932a7ac983
Jirles <emily.jirles@gmail.com>
2018-11-21 12:48:00 -0500

    Add documentation to EMAIL

--- src/functions/EMAIL.ts
+++ src/functions/EMAIL.ts
@@ -1,5 +1,9 @@
 import { CONFIG } from "./CONFIG"
 
+/**
+ * Returns the user's email from the configuration object.
+ */
+
 export default function EMAIL(): string|undefined {
   return CONFIG().userEmail || undefined
 }
