4a82f80ba6439be3adf02fcdff32cca124584d3f
Jirles <emily.jirles@gmail.com>
2018-12-19 15:55:12 -0500

    Add percentage, significantDigits, min/max digits tests to FORMATNUMBER test suite

--- src/functions/__tests__/FORMATNUMBER.test.ts
+++ src/functions/__tests__/FORMATNUMBER.test.ts
@@ -9,32 +9,64 @@ beforeEach(() => {
 })
 
 test("formats a number value according to a locale and options passed in", () => {
-  expect(FORMATNUMBER(1 / 3)).toBe("0.333")
-  expect(FORMATNUMBER(1 / 3, "pt-BR")).toBe("0,333")
-  expect(FORMATNUMBER(10000 / 3, "pt-BR")).toBe("3.333,333")
-  expect(FORMATNUMBER(10000 / 3, "fr-FR")).toBe("3 333,333")
-  expect(FORMATNUMBER(10000 / 3, "pt-BR", { useGrouping: false })).toBe("3333,333")
-  expect(FORMATNUMBER(10000 / 3, "fr-FR", { useGrouping: false })).toBe("3333,333")
-  expect(FORMATNUMBER(1 / 3, null, { minimumFractionDigits: 5 })).toBe("0.33333")
-  expect(FORMATNUMBER(1 / 3, null, { maximumFractionDigits: 2 })).toBe("0.33")
-  expect(FORMATNUMBER(1 / 3, null, { minimumIntegerDigits: 2 })).toBe("00.333")
-  expect(FORMATNUMBER(10000 / 3, "pt-BR", { style: "currency", currency: "BRL" })).toBe("R$3.333,33")
-  expect(FORMATNUMBER(10000 / 3, "fr-FR", { style: "currency", currency: "EUR" })).toBe("3 333,33 €")
+  expect(FORMATNUMBER(1 / 3)).toEqual("0.333")
+  expect(FORMATNUMBER(1 / 3, "pt-BR")).toEqual("0,333")
+  expect(FORMATNUMBER(10000 / 3, "pt-BR")).toEqual("3.333,333")
+  expect(FORMATNUMBER(10000 / 3, "fr-FR")).toEqual("3 333,333")
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { useGrouping: false })).toEqual("3333,333")
+  expect(FORMATNUMBER(10000 / 3, "fr-FR", { useGrouping: false })).toEqual("3333,333")
+  expect(FORMATNUMBER(1 / 3, null, { minimumFractionDigits: 5 })).toEqual("0.33333")
+  expect(FORMATNUMBER(1 / 3, null, { maximumFractionDigits: 2 })).toEqual("0.33")
+  expect(FORMATNUMBER(1 / 3, null, { minimumIntegerDigits: 2 })).toEqual("00.333")
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { style: "currency", currency: "BRL" })).toEqual("R$3.333,33")
+  expect(FORMATNUMBER(10000 / 3, "fr-FR", { style: "currency", currency: "EUR" })).toEqual("3 333,33 €")
 })
 
 test("formats a number for different locales", () => {
   CONFIGURE({ currencyCode: "EUR" }) // number formatting for the Euro in different locales
-  expect(FORMATNUMBER(10000 / 3, "pt-BR", { style: "currency" })).toBe("€3.333,33")
-  expect(FORMATNUMBER(10000 / 3, "fr-FR", { style: "currency" })).toBe("3 333,33 €")
-  expect(FORMATNUMBER(10000 / 3, "en-US", { style: "currency" })).toBe("€3,333.33")
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { style: "currency" })).toEqual("€3.333,33")
+  expect(FORMATNUMBER(10000 / 3, "fr-FR", { style: "currency" })).toEqual("3 333,33 €")
+  expect(FORMATNUMBER(10000 / 3, "en-US", { style: "currency" })).toEqual("€3,333.33")
 
   CONFIGURE({ currencyCode: "BRL" }) // number formatting for the Brazilian real in different locales
-  expect(FORMATNUMBER(10000 / 3, "pt-BR", { style: "currency" })).toBe("R$3.333,33")
-  expect(FORMATNUMBER(10000 / 3, "fr-FR", { style: "currency" })).toBe("3 333,33 R$")
-  expect(FORMATNUMBER(10000 / 3, "en-US", { style: "currency" })).toBe("R$3,333.33")
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { style: "currency" })).toEqual("R$3.333,33")
+  expect(FORMATNUMBER(10000 / 3, "fr-FR", { style: "currency" })).toEqual("3 333,33 R$")
+  expect(FORMATNUMBER(10000 / 3, "en-US", { style: "currency" })).toEqual("R$3,333.33")
 
   CONFIGURE({ currencyCode: "USD" }) // number formatting for the US dollar in different locales
-  expect(FORMATNUMBER(10000 / 3, "pt-BR", { style: "currency" })).toBe("US$3.333,33")
-  expect(FORMATNUMBER(10000 / 3, "fr-FR", { style: "currency" })).toBe("3 333,33 $US")
-  expect(FORMATNUMBER(10000 / 3, "en-US", { style: "currency" })).toBe("$3,333.33")
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { style: "currency" })).toEqual("US$3.333,33")
+  expect(FORMATNUMBER(10000 / 3, "fr-FR", { style: "currency" })).toEqual("3 333,33 $US")
+  expect(FORMATNUMBER(10000 / 3, "en-US", { style: "currency" })).toEqual("$3,333.33")
+})
+
+test("formats percentages", () => {
+  expect(FORMATNUMBER(0.21, "en-US", { style: "percent" })).toEqual("21%")
+  expect(FORMATNUMBER(0.21, "fr-FR", { style: "percent" })).toEqual("21 %")
+})
+
+test("allows user to choose number of significant digits", () => {
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { maximumSignificantDigits: 5 })).toEqual("3.333,3")
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { minimumSignificantDigits: 6 })).toEqual("3.333,33")
+  expect(FORMATNUMBER(10000 / 3, "fr-FR", { maximumSignificantDigits: 10 })).toEqual("3 333,333333")
+})
+
+test("minimumSignificantDigits and maximumSignificantDigits are forced to fit the range 1-21", () => {
+  // maximumSignificantDigits can be no greater than 21
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { maximumSignificantDigits: 21 })).toEqual("3.333,33333333333")
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { maximumSignificantDigits: 22 })).toEqual("3.333,33333333333")
+  // minimumSignificantDigits can be no greater than 21
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { minimumSignificantDigits: 21 })).toEqual("3.333,33333333333000000")
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { minimumSignificantDigits: 22 })).toEqual("3.333,33333333333000000")
+  // minimumSignificantDigits can be no less 1
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { minimumSignificantDigits: -1 })).toEqual("3.000")
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { minimumSignificantDigits: 0 })).toEqual("3.000")
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { minimumSignificantDigits: 1 })).toEqual("3.000")
+  // minimumSignificantDigits can be no less 1
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { maximumSignificantDigits: 0 })).toEqual("3.000")
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { maximumSignificantDigits: -1 })).toEqual("3.000")
+  expect(FORMATNUMBER(10000 / 3, "pt-BR", { maximumSignificantDigits: 1 })).toEqual("3.000")
+})
+
+test("returns 'NaN' if not value is passed in", () => {
+  expect(FORMATNUMBER(null, "pt-BR", { style: "currency" })).toEqual("NaN")
 })
