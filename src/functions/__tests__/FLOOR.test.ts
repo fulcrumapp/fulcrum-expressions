aae1686dc0f66087ddba8fdee348746105d8c7f1
Jirles <emily.jirles@gmail.com>
2018-12-19 13:16:51 -0500

    Add test for multiple = 0, fix some test suite spacing

--- src/functions/__tests__/FLOOR.test.ts
+++ src/functions/__tests__/FLOOR.test.ts
@@ -1,15 +1,16 @@
 import FLOOR from "../FLOOR"
 
 test("returns number rounded down, towards zero, to the nearest multiple", () => {
-    expect(FLOOR(1)).toBe(1)
-    expect(FLOOR(1.5)).toBe(1)
-    expect(FLOOR(2.5)).toBe(2)
-    expect(FLOOR(-45)).toBe(-45)
-    expect(FLOOR(2.3333333, 4)).toBe(0)
-    expect(FLOOR(0.13, 0.25)).toBe(0)
-    expect(FLOOR(0.31, 0.25)).toBe(0.25)
-    expect(FLOOR(-0.13, 0.25)).toBe(-0.25)
-    expect(FLOOR(-0.31, 0.25)).toBe(-0.5)
+  expect(FLOOR(1)).toBe(1)
+  expect(FLOOR(1.5)).toBe(1)
+  expect(FLOOR(2.5)).toBe(2)
+  expect(FLOOR(-45)).toBe(-45)
+  expect(FLOOR(2.3333333, 4)).toBe(0)
+  expect(FLOOR(0.13, 0.25)).toBe(0)
+  expect(FLOOR(0.31, 0.25)).toBe(0.25)
+  expect(FLOOR(-0.13, 0.25)).toBe(-0.25)
+  expect(FLOOR(-0.31, 0.25)).toBe(-0.5)
+  expect(FLOOR(0.31, 0)).toBe(0)
 })
 
 test("it accepts a number as a string", () => {
