5b18b20d4a3e765ef01e732c467590e01c31bf8b
Jirles <emily.jirles@gmail.com>
2018-12-20 16:09:39 -0500

    Add test to check for param validation

--- src/functions/__tests__/ON.test.ts
+++ src/functions/__tests__/ON.test.ts
@@ -34,3 +34,53 @@ test("it raises an error if invalid params are passed", () => {
   // @ts-ignore: Checking for non-string being used for target
   expect(() => ON("click", {}, jest.fn())).toThrow()
 })
+
+test("it validates event params", () => {
+  $$runtime.elementsByDataName = {
+    "my_field": {
+      data_name: "my_field",
+      disabled: false,
+      hidden: false,
+      key: "abcd",
+      label: "My Hyperlink Field",
+      required: false,
+      type: "HyperlinkField",
+    },
+    "repeatable": {
+      data_name: "repeatable",
+      disabled: false,
+      hidden: false,
+      key: "abcd",
+      label: "My Repeatable Field",
+      required: false,
+      type: "Repeatable",
+      elements: [
+        { data_name: "child_photo_field",
+          key: "98ui",
+          type: "PhotoField",
+          required: false,
+          disabled: false,
+          hidden: false,
+          label: "My Photo Field",
+        }
+      ]
+    },
+    "child_photo_field": { 
+      data_name: "child_photo_field",
+      key: "98ui",
+      type: "PhotoField",
+      required: false,
+      disabled: false,
+      hidden: false,
+      label: "My Photo Field",
+    },
+  }
+
+  expect(() => ON("click", "child_photo_field", jest.fn())).toThrow("Invalid usage of ON(): \"child_photo_field\" is not a valid field for the \"click\" event")
+  expect(() => ON("validate-repeatable", "my_field", jest.fn())).toThrow()
+  expect(() => ON("change-geometry", "child_photo_field", jest.fn())).toThrow()
+  expect(() => ON("remove-photo", "repeatable", jest.fn())).toThrow()
+  expect(() => ON("remove-audio", "my_field", jest.fn())).toThrow()
+  expect(() => ON("remove-video", "child_photo_field", jest.fn())).toThrow()
+  expect(() => ON("change", "@project", jest.fn())).not.toThrow()
+})
