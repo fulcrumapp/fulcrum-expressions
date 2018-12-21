120f66b3cf870a359eba3340c88a08a38269aa96
Jirles <emily.jirles@gmail.com>
2018-12-21 11:25:31 -0500

    Add additional checks to OFF test suite

--- src/functions/__tests__/OFF.test.ts
+++ src/functions/__tests__/OFF.test.ts
@@ -3,21 +3,25 @@ import Runtime from "../../runtime/index"
 
 test("it removes an event handler", () => {
   const runtime = new Runtime()
-  const callback = () => { return true }
+  const myCallback = () => { return true }
+  const jestCallback = jest.fn()
   runtime.events = {
     change: {
-      __no_param: [ callback ],
-      my_field: [ callback ]
+      __no_param: [ myCallback ],
+      my_field: [ myCallback, jestCallback ]
     },
     blur: {
-      my_other_field: [ callback ]
+      my_other_field: [ myCallback, jestCallback ]
     }
   }
 
-  OFF("change", "my_field", callback)
-  // @ts-ignore 'change' is not a FormFieldEvent, but works for this simple test
-  OFF("change", callback)
-  expect(runtime.events.change).toEqual({ __no_param: [], my_field: [] })
+  OFF("change", "my_field", myCallback)
+  // @ts-ignore 'change' is not recognized as a FormFieldEvent, but works for this simple test
+  OFF("change", myCallback)
+  // @ts-ignore 'blur' is not recognized as a FormFieldEvent, but works for this simple test
+  OFF("blur", "my_other_field")
+  expect(runtime.events.change).toEqual({ __no_param: [], my_field: [ jestCallback ] })
+  expect(runtime.events.blur.my_other_field).toEqual([])
 })
 
 test("throws an error if the first or second parameter are not strings", () => {
