519e8acbdaf729e67a9af1a899ba4ee32ee39d29
Jirles <emily.jirles@gmail.com>
2018-12-27 14:01:55 -0500

    Add getItem call tests for no-ops

--- src/host/__tests__/storage.test.ts
+++ src/host/__tests__/storage.test.ts
@@ -22,9 +22,25 @@ describe("setItem call", () => {
   })
 })
 
-test("it proxies the getItem call to the runtime", () => {
-  const mock = $$runtime.$$storageGetItem = jest.fn().mockReturnValue("test_return")
-  expect(storage.getItem("test")).toEqual("test_return")
+describe("getItem call", () => {
+  test("it proxies the getItem call to the runtime", () => {
+    const mock = $$runtime.$$storageGetItem = jest.fn().mockReturnValue("test_return") 
+    expect(storage.getItem("test")).toEqual("test_return")
+    expect(mock).toHaveBeenCalled()
+  })
+
+  test("getItem will no-op if no key is passed in", () => {
+    // @ts-ignore Null value key to return undefined
+    expect(storage.getItem(null)).toBeUndefined()
+  })
+
+  test("getItem will no-op if $$storageGetItem does not exist on runtime", () => {
+    $$runtime.$$storageSetItem = jest.fn()
+    storage.setItem("foo", "{ bar: 'baz' }")
+    // prepareRuntime does not set up $$storageGetItem, host does
+    // just calling getItem here will be sufficient to no-op
+    expect(storage.getItem("foo")).toBeUndefined()
+  })
 })
 
 test("it proxies the clear call to the runtime", () => {
