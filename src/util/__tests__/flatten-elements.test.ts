d100f621d9ea953e468b828d0ec5644859d277c1
Jirles <emily.jirles@gmail.com>
2019-01-02 12:36:46 -0500

    Add check to make sure flattenElements doesn't attempt to recursively flatten an empty section or repeatable

--- src/util/__tests__/flatten-elements.test.ts
+++ src/util/__tests__/flatten-elements.test.ts
@@ -1,7 +1,7 @@
 import flattenElements from "../flatten-elements"
 import repeatable from "../../test/fixtures/repeatable"
 
-test("flattens elements to one level deep", () => {
+test("flattens form elements to one level deep", () => {
   const flattenedArray = flattenElements([repeatable], true, false, undefined, false)
   expect(flattenedArray.length).toBe(5)
   expect(flattenedArray[1]).toEqual(repeatable.elements[0])
@@ -9,7 +9,40 @@ test("flattens elements to one level deep", () => {
 
 test("can assign a parent element to elements in array", () => {
   // @ts-ignore repeatable here is a stand-in for a FormField
-  const flattenedArray = flattenElements([repeatable], true, true, "World's Best Parent", false)
+  const flattenedArray = flattenElements([repeatable], true, true, undefined, false)
   expect(flattenedArray.length).toBe(5)
-  expect(flattenedArray[0].parent).toEqual("World's Best Parent")
+  // flattenElements sets parent to repeatable element in child records
+  expect(flattenedArray[1].parent).toEqual(flattenedArray[0])
+})
+
+test("won't attempt to recurseively flatten an empty section or repeatable", () => {
+  const mockedFlatten = jest.spyOn(flattenElements, "apply")
+  // add an empty section (not elements key) to repeatable.elements
+  repeatable.elements.push({
+    data_name: "my_section",
+    default_value: null,
+    description: null,
+    disabled: false,
+    format: "",
+    hidden: false,
+    key: "18uh",
+    label: "My Section",
+    max: null,
+    max_length: null,
+    min: null,
+    min_length: null,
+    numeric: true,
+    required: false,
+    required_conditions: null,
+    required_conditions_type: null,
+    type: "Section",
+    visible_conditions: null,
+    visible_conditions_type: null,
+  })
+  // @ts-ignore explicit any for `this`
+  const flattenedArray = flattenElements.apply(this, [[repeatable], true, false, undefined, true])
+  expect(flattenedArray.length).toBe(6)
+  // initial call above + 2 recursive calls (repeatables.elements and nested repeatable child_items) = 3 calls 
+  // should not call a 4th time for the empty section
+  expect(mockedFlatten).toHaveBeenCalledTimes(3)
 })
