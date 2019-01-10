13448033c4d82543346501d7139c20554614821c
Jirles <emily.jirles@gmail.com>
2019-01-10 11:42:38 -0500

    Expand RPAD test suite, minor cosmetic spacing for function

--- src/functions/RPAD.ts
+++ src/functions/RPAD.ts
@@ -1,11 +1,11 @@
-import { isNumber, isString, isUndefined } from "lodash"
+import { isFinite, isString, isUndefined } from "lodash"
 import FLOOR from "./FLOOR"
 import LEFT from "./LEFT"
 
 /**
  * Returns a string padded to the right by a desired character.
  * @param value required; string value to be padded
- * @param count required; number of padded spaces desired
+ * @param count required; total number of spaces desired in returned value
  * @param padding optional; character with which the `value` is to be padded; defaults to `" "`
  * @returns a padded string value
  * 
@@ -18,9 +18,9 @@ export default function RPAD(value: string, count: number, padding?: string): st
 export default function RPAD(value: any, count: any, padding?: any): string|undefined
 export default function RPAD(value: any, count: any, padding=' '): string|undefined {
   count  = FLOOR(count)
-  value  = isUndefined(value) ? "": value.toString()
+  value  = isUndefined(value) ? "" : value.toString()
 
-  if (!isNumber(count) || !isString(padding)) { return }
+  if (!isFinite(count) || !isString(padding)) { return }
 
   return LEFT(value + Array(count).join(padding), count)
 }
