7fe610c80979a7b16992338ebf053ceb25ed0941
Jirles <emily.jirles@gmail.com>
2019-01-10 11:08:11 -0500

    Port RPAD to ts, add documentation

---
+++ src/functions/RPAD.ts
@@ -0,0 +1,26 @@
+import { isNumber, isString, isUndefined } from "lodash"
+import FLOOR from "./FLOOR"
+import LEFT from "./LEFT"
+
+/**
+ * Returns a string padded to the right by a desired character.
+ * @param value required; string value to be padded
+ * @param count required; number of padded spaces desired
+ * @param padding optional; character with which the `value` is to be padded; defaults to `" "`
+ * @returns a padded string value
+ * 
+ * @example
+ * 
+ * RPAD('1', 2, '0') // returns "10"
+ */
+
+export default function RPAD(value: string, count: number, padding?: string): string
+export default function RPAD(value: any, count: any, padding?: any): string|undefined
+export default function RPAD(value: any, count: any, padding=' '): string|undefined {
+  count  = FLOOR(count)
+  value  = isUndefined(value) ? "": value.toString()
+
+  if (!isNumber(count) || !isString(padding)) { return }
+
+  return LEFT(value + Array(count).join(padding), count)
+}
