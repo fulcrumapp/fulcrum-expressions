e0bda58ae80ee65b9cc224dd780e1a21adb7aa9a
Jirles <emily.jirles@gmail.com>
2019-01-11 13:21:57 -0500

    Add SETASSIGNMENT test suite

---
+++ src/functions/__tests__/SETASSIGNMENT.test.ts
@@ -0,0 +1,25 @@
+import form from "../../test/fixtures/form"
+import { prepareRuntime } from "../../test/helpers"
+import { SetValueResult } from "../../types/results"
+import SETASSIGNMENT from "../SETASSIGNMENT"
+
+
+beforeEach(() => {
+  prepareRuntime()
+})
+
+test("assigns a user to record", () => {
+  SETASSIGNMENT("John Jacob Jingleheimer Schmidt")
+  const resultValue = $$runtime.results[0]
+  const expectedValue: SetValueResult = {
+    key: "@assignment",
+    type: "set-value",
+    value: "\"John Jacob Jingleheimer Schmidt\"",
+  }
+
+  expect(resultValue).toEqual(expectedValue)
+})
+
+test("throws an error if not passed a user name as a string", () => {
+  expect(() => SETASSIGNMENT(1234)).toThrow("user must be a string")
+})
