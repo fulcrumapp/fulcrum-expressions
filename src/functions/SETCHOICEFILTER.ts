05bc6817974edf3f67dd37e0de3810c117378be8
Jirles <emily.jirles@gmail.com>
2019-01-14 15:06:33 -0500

    Convert SETCHOICEFILTER to TS, add documentation

---
+++ src/functions/SETCHOICEFILTER.ts
@@ -0,0 +1,16 @@
+import { chain, isUndefined, isNull } from "lodash"
+import SETFORMATTRIBUTES from "./SETFORMATTRIBUTES"
+
+/**
+ * Sets a choice filter for a form.
+ * @param dataName required; data name of field to be updated
+ * @param value required; a value or an array of values on which to filter
+ */
+
+export default function SETCHOICEFILTER(dataName: string, value: any[]): void
+export default function SETCHOICEFILTER(dataName: string, value: any): void
+export default function SETCHOICEFILTER(dataName: string, value: any): void {
+  const filterValue = isUndefined(value) || isNull(value) ? null : chain([value]).flatten().compact().map(String).value()
+
+  SETFORMATTRIBUTES(dataName, { choice_filter: filterValue })
+}
