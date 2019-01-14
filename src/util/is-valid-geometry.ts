18eae1aae59fdf1ab340b1f5e7cbe832076c6eb7
Jirles <emily.jirles@gmail.com>
2019-01-14 17:32:22 -0500

    Convert isValidGeometry to TS, add documentation

---
+++ src/util/is-valid-geometry.ts
@@ -0,0 +1,28 @@
+import { isArray, isFinite, isObject, isNull, isUndefined, some } from "lodash"
+
+interface ValidGeometry {
+  type: "Point",
+  coordinates: number[]
+}
+
+/**
+ * Checks to see if a valid GeoJSON object was passed in.
+ * @param geometry geometry object to be tested
+ * @returns boolean value
+ */
+
+export default function isValidGeometry(geometry: ValidGeometry): boolean
+export default function isValidGeometry(geometry: any): boolean
+export default function isValidGeometry(): boolean
+export default function isValidGeometry(geometry?: any): boolean {
+  if (isUndefined(geometry) || isNull(geometry)) { return true }
+
+  if (!isObject(geometry) || 
+      geometry.type !== "Point" ||
+      !isArray(geometry.coordinates) ||
+      geometry.coordinates.length !== 2 ||
+      some(geometry.coordinates, (coord) => !isFinite(coord))
+      ) { return false }
+
+  return true
+}
