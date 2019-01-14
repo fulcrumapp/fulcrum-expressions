0369c5c062195847a6b74e26a993d89e909ba08a
Jirles <emily.jirles@gmail.com>
2019-01-14 10:45:31 -0500

    Expand SETFORMATTRIBUTES test suite

--- src/functions/__tests__/SETFORMATTRIBUTES.test.ts
+++ src/functions/__tests__/SETFORMATTRIBUTES.test.ts
@@ -49,3 +49,57 @@ test("will update multiple attributes at once", () => {
   expect($$runtime.results[0]).toEqual(expectedResults[0])
   expect($$runtime.results[2]).toEqual(expectedResults[2])
 })
+
+test("accepts @status as a dataName to update the status", () => {
+  const expectedResult = {
+    type: "update-element",
+    key: "@status",
+    attribute: "disabled",
+    value: JSON.stringify(true),
+  }
+
+  SETFORMATTRIBUTES("@status", { disabled: true})
+  expect($$runtime.results[0]).toEqual(expectedResult)
+})
+
+test("sets a form-level attribute for a field", () => {
+  const expectedResult = {
+    type: "update-element",
+    key: "97ab",
+    attribute: "required",
+    value: JSON.stringify(true),
+  }
+  // @ts-ignore Ignore FormFields type check for test
+  $$runtime.elementsByDataName = { name: form.elements[0] }
+
+  SETFORMATTRIBUTES("name", { required: true })
+  expect($$runtime.results[0]).toEqual(expectedResult)
+})
+
+test("attribute defaults to null if it is undefined or null", () => {
+  const expectedResult = {
+    type: "update-element",
+    key: "97ab",
+    attribute: "required",
+    value: null,
+  }
+  // @ts-ignore Ignore FormFields type check for test
+  $$runtime.elementsByDataName = { name: form.elements[0] }
+
+  SETFORMATTRIBUTES("name", { required: undefined })
+  SETFORMATTRIBUTES("name", { required: null })
+
+  expect($$runtime.results[0]).toEqual(expectedResult)
+  expect($$runtime.results[1]).toEqual(expectedResult)  
+})
+
+test("will not update if field does not exist and data name isn't @form or @status", () => {
+  expect(SETFORMATTRIBUTES("bad_field", { required: true })).toBeUndefined()
+})
+
+test("will not update if attribute is not among allowable FORM_ATTRIBUTES", () => {
+  // @ts-ignore Ignore FormFields type check for test
+  $$runtime.elementsByDataName = { name: form.elements[0] }
+
+  expect(SETFORMATTRIBUTES("name", { geography_required: true })).toBeUndefined()
+})
