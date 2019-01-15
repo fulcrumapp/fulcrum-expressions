51c0e55cd803ab0566588545de6c166a842932d7
Jirles <emily.jirles@gmail.com>
2019-01-15 16:28:07 -0500

    Add SETREQUIRED test suite

---
+++ src/functions/__tests__/SETREQUIRED.test.ts
@@ -0,0 +1,22 @@
+import { prepareRuntime } from "../../test/helpers"
+import form from "../../test/fixtures/form"
+import SETREQUIRED from "../SETREQUIRED"
+
+test("sets a field to required", () => {
+  prepareRuntime()
+  // @ts-ignore FormFields check for tests
+  $$runtime.elementsByDataName.items = form.elements[1]
+
+  const expectedResult = {
+    type: "update-element",
+    key: "1337",
+    attribute: "required",
+    value: "true",
+  }
+
+  SETREQUIRED("items", true)
+  SETREQUIRED("items")
+  expect($$runtime.results[0]).toEqual(expectedResult)
+  // @ts-ignore value attribute will exists after SETHIDDEN is called
+  expect($$runtime.results[1].value).toBeNull()
+})
