593b4c59202363bbd8846a37289a11a4d6bca3e5
Jirles <emily.jirles@gmail.com>
2019-01-16 14:04:44 -0500

    Convert SIN to ts, add documentation

---
+++ src/functions/SIN.ts
@@ -0,0 +1,15 @@
+import NUM from "./NUM"
+
+/**
+ * Returns the sine of the specified angle value, which must be specified in radians.
+ * @param value numeric value specifying radians
+ * @returns numeric value between 1 and -1 indicating the angle's sine
+ * @example
+ * SIN(12) // returns -0.5365729180004349
+ */
+
+export default function SIN(value: number): number
+export default function SIN(value: string): number
+export default function SIN(value: any): number {
+  return Math.sin(NUM(value))
+}
