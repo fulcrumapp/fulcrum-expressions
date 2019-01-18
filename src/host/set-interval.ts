ebd1615365003100fa2c49cf4275fd4453212892
Jirles <emily.jirles@gmail.com>
2019-01-18 17:19:55 -0500

    Add setTimeout function to setInterval method, edit documentation

--- src/host/set-interval.ts
+++ src/host/set-interval.ts
@@ -1,27 +1,27 @@
 import { isUndefined } from "lodash"
 import { state } from "./state"
+import setTimeout from "./set-timeout"
 
 /**
- * Sets a interval at which to call a specified callback.
+ * The SETINTERVAL function can be used to repeatedly call a function at a specified interval. 
+ * It’s nearly identical to the web platform standard setTimeout.
  * @param callback required; function to be called
- * @param interval required; numeric value indicating number of seconds between subsequent calls
+ * @param interval required; numeric value indicating number of milliseconds between subsequent calls
  * @returns numeric id interval on host
  */
 
-export default function setInterval(callback: Function, interval: number): number {
-  const hostSetTimeout = $$runtime.$$setTimeout
-
-  if (!isUndefined(hostSetTimeout)) {
+export default function setInterval(callback: Function, interval: number): number|undefined {
+  if (!isUndefined($$runtime.$$setTimeout)) {
     const id = ++state().nextIntervalID
 
     const wrapper = () => {
       if (state().intervals[id]) {
-        state().intervals[id] = () => hostSetTimeout(wrapper, interval)
+        state().intervals[id] = () => setTimeout(wrapper, interval)
         callback()
       }
     }
 
-    state().intervals[id] = () => hostSetTimeout(wrapper, interval)
+    state().intervals[id] = () => setTimeout(wrapper, interval)
 
     return id
   }
