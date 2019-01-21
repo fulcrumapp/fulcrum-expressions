86062533806f62e76b053080f133f277cd705231
Jirles <emily.jirles@gmail.com>
2019-01-21 14:29:28 -0500

    Fix setInterval test to actually reflect implementation

--- src/host/__tests__/set-interval.test.ts
+++ src/host/__tests__/set-interval.test.ts
@@ -11,13 +11,12 @@ test("sets a function to be called after a certain duration", () => {
   const duration = 100
   const callback = jest.fn()
   const mockedAsync = $$runtime.invokeAsync = jest.fn()
+  $$runtime.$$setTimeout = jest.fn()
   const hostState = state()
 
   expect(hostSetInterval(callback, duration)).toBe(1)
   expect(hostState.nextIntervalID).toEqual(1)
   expect(hostState.intervals[1]).toBeDefined()
-
-  hostState.intervals[1]()
   expect(mockedAsync).toHaveBeenCalled()
 })
 
