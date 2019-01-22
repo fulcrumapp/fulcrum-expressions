b63d27f4d78e158a17991d1ea6bd7f7b7ee28055
Jirles <emily.jirles@gmail.com>
2019-01-22 15:19:40 -0500

    Add new toContainAllElements matcher to SHUFFLE test

--- src/functions/__tests__/SHUFFLE.test.ts
+++ src/functions/__tests__/SHUFFLE.test.ts
@@ -1,7 +1,12 @@
+import { extendContainAllElements } from "../../test/helpers"
 import SHUFFLE from "../SHUFFLE"
 
+beforeEach(extendContainAllElements)
+
 test("returns an array of shuffled values", () => {
-  expect(SHUFFLE([1, 2, 3, 4])).not.toEqual([1, 2, 3, 4])
+  // @ts-ignore toContainAllElements will exist after extend call
+  expect(SHUFFLE([1, 2, 3, 4], 5)).toContainAllElements([1, 2, 3, 4, 5])
   expect(SHUFFLE()).toEqual([])
-  expect(SHUFFLE(1, "test", 34, "test")).not.toEqual([1, "test", 34, "test"])
+  // @ts-ignore toContainAllElements will exist after extend call
+  expect(SHUFFLE(1, "test", 34, "test")).toContainAllElements([1, "test", 34, "test"])
 })
